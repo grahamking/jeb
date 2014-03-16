e4c43df539f3c3babce7efe4f1edc3eff6e7eda5
Graham King <graham@gkgk.org>
2014-03-16 13:18:30 -0700

    Logging, functions, and start of step over

--- server.go
+++ server.go
@@ -1,11 +1,13 @@
 package main
 
 import (
+	"errors"
 	"github.com/nsf/termbox-go"
 	"io"
 	"io/ioutil"
 	"log"
 	"net"
+	"os"
 	"strconv"
 	"strings"
 )
@@ -13,6 +15,8 @@ import (
 func runServer() {
 	termbox.Init()
 	defer termbox.Close()
+
+	setupLogging()
 
 	l, err := net.Listen("unix", "/tmp/jeb-socket")
 	if err != nil {
@@ -26,53 +30,126 @@ func runServer() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	for {
-		buf := make([]byte, 1024)
-		numRead, err := c.Read(buf)
-		if err == io.EOF {
-			break
-		}
-		if err != nil {
-			log.Fatal(err)
-		}
-		parts := strings.Split(string(buf[:numRead]), ":")
-		if len(parts) != 3 {
-			log.Fatalf("Expected 3 parts, got %v", parts)
-		}
 
-		filename := parts[0]
-		line, err := strconv.Atoi(parts[1])
-		if err != nil {
-			log.Fatal(err)
-		}
-		//function := parts[2]
-		display(filename, line)
-		waitForInput()
-		c.Write([]byte{'\n'})
-	}
-	c.Close()
+	log.Println("NewJeb")
+	j := NewJeb(c)
+	log.Println("j.run")
+	j.run()
+
 	l.Close()
 }
 
-func waitForInput() {
-loop:
-	for {
-		switch ev := termbox.PollEvent(); ev.Type {
-		case termbox.EventKey:
-			break loop
-		default:
-			continue
-		}
-	}
-}
-
-func display(filename string, lineNum int) {
-	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
-	filedata, err := ioutil.ReadFile(filename)
+func setupLogging() {
+	l, err := os.Create("jeb.log")
 	if err != nil {
 		log.Fatal(err)
 	}
-	lines := strings.Split(string(filedata), "\n")
+	log.SetOutput(l)
+}
+
+type Jeb struct {
+	c              net.Conn
+	buf            []byte
+	files          map[string][]string
+	skipInFunction string
+}
+
+func NewJeb(c net.Conn) *Jeb {
+	return &Jeb{
+		c:     c,
+		buf:   make([]byte, 1024),
+		files: make(map[string][]string),
+	}
+}
+
+func (j *Jeb) run() {
+	for {
+		log.Println("receive")
+		numRead := j.receive()
+		if numRead == 0 {
+			break
+		}
+
+		log.Println("parse")
+		filename, line, function := j.parse(numRead)
+
+		if j.skipInFunction == "" || j.skipInFunction == function {
+			log.Println("display: ", filename, line)
+			j.display(filename, line)
+
+			input, err := j.waitForInput()
+			log.Println("input: ", input)
+			if err != nil {
+				break
+			}
+			if input == 's' {
+				// step in
+				j.skipInFunction = ""
+			} else if input == 'n' {
+				// next (step over)
+				j.skipInFunction = function
+			}
+
+		}
+		log.Println("proceed")
+		j.proceed()
+	}
+	j.c.Close()
+}
+
+func (j *Jeb) receive() int {
+	numRead, err := j.c.Read(j.buf)
+	if err == io.EOF {
+		return 0
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return numRead
+}
+
+func (j *Jeb) parse(numRead int) (filename string, line int, function string) {
+	var err error
+	parts := strings.Split(string(j.buf[:numRead]), ":")
+	if len(parts) != 3 {
+		log.Fatalf("Expected 3 parts, got %v", parts)
+	}
+	filename = parts[0]
+	line, err = strconv.Atoi(parts[1])
+	if err != nil {
+		log.Fatal(err)
+	}
+	function = parts[2]
+
+	return filename, line, function
+}
+
+// proceed tells client to continue to next line
+func (j *Jeb) proceed() {
+	j.c.Write([]byte{'\n'})
+}
+
+func (j *Jeb) waitForInput() (rune, error) {
+	ev := termbox.PollEvent()
+	for ev.Type != termbox.EventKey {
+		ev = termbox.PollEvent()
+		log.Println("Event is", ev)
+	}
+	if ev.Key == termbox.KeyCtrlC {
+		return 0, errors.New("Ctrl-C")
+	}
+	return ev.Ch, nil
+}
+
+func (j *Jeb) display(filename string, lineNum int) {
+	termbox.Clear(termbox.ColorDefault, termbox.ColorDefault)
+
+	lines, ok := j.files[filename]
+	if !ok {
+		j.load(filename)
+		lines, _ = j.files[filename]
+	}
+
 	for i, line := range lines {
 		if i+1 == lineNum {
 			tbprint(0, i, termbox.ColorDefault|termbox.AttrReverse, termbox.ColorDefault, line)
@@ -81,6 +158,17 @@ func display(filename string, lineNum int) {
 		}
 	}
 	termbox.Flush()
+}
+
+// load filename as array of file lines into Jeb.files
+func (j *Jeb) load(filename string) {
+	filedata, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal(err)
+	}
+	lines := strings.Split(string(filedata), "\n")
+
+	j.files[filename] = lines
 }
 
 func bold(str string) string {
